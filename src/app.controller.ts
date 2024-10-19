b09c55615d01246a82a7c6b980669d55b34d3945
echelon35 <kbrun.pro@gmail.com>
2024-10-19 19:29:24 +0200

    Healthcheck

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './Common/Decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  @Public()
+  getHealth(): boolean {
+    return true;
   }
 }
